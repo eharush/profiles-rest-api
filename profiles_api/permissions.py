00daa7dc36e99d0221d99468fc26589aadd0817b
Eran Harush <e_harush@yahoo.com>
2020-10-29 14:30:39 +0200

    Added login API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -6,7 +6,6 @@ class UpdateOwnProfile(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profile"""
-        print(f'request.method = {request.method}')
         if request.method in permissions.SAFE_METHODS:
             return True
 
